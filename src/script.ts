45edaee5c19d1114e5cbd7f2d1be5c206a02a569
geovanni.cardoso <geovanni.moreira@gmail.com>
2022-09-16 16:05:19 -0300

    exerc- calss

---
+++ src/script.ts
@@ -0,0 +1,18 @@
+class Client{
+    public nome : string
+    public idade : number
+    public peso : number
+    public altura : string
+}
+const cliente =  new Client();
+
+const nomeClient = cliente.nome = 'Geovanni'
+const idadeClient = cliente.idade = 25   
+const pesoCliente = cliente.peso = 80
+const alturaCliente = cliente.altura = '1.77'
+
+console.log(`Qual o nome do cliente: ${nomeClient}\n`);
+console.log(`Qual a idade do cliente: ${idadeClient}\n`);
+console.log(`Qual o peso do cliente: ${pesoCliente}\n`);
+console.log(`Qual á altura do cliente: ${alturaCliente}\n`);
+ 
